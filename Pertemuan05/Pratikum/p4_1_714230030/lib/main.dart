5611a1319c2e8d85133e96172acf65fcccc1f17f
rendykml <rendikamaluddin2018@gmail.com>
2025-11-12 11:32:23 +0700

    edit pertemuan 05

--- Pertemuan05/Pratikum/p4_1_714230030/lib/main.dart
+++ Pertemuan05/Pratikum/p4_1_714230030/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Tempat Wisata Bandung',
       theme: ThemeData(useMaterial3: true),
       home: const MainScreen(),
